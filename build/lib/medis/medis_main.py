e87cb363ae7e378bd18a1532636a896000e88027
rdodkins <rupertdodkins@gmail.com>
2020-08-13 11:21:24 -0700

    Added the build pip stuff to this branch

--- build/lib/medis/medis_main.py
+++ build/lib/medis/medis_main.py
@@ -27,7 +27,8 @@ from pprint import pprint
 
 from medis.telescope import Telescope
 from medis.MKIDS import Camera
-from medis.params import sp, ap, tp, iop, atmp, cdip, mp
+from medis.params import sp, ap, tp, iop, atmp, mp
+from medis.CDI import cdi
 
 ################################################################################################################
 ################################################################################################################
@@ -68,7 +69,7 @@ class RunMedis():
 
         iop.update_testname(self.name)
         # for storing a checking between tests
-        self.params = {'ap': ap, 'tp': tp, 'atmp': atmp, 'cdip': cdip, 'iop': iop, 'sp': sp, 'mp': mp}
+        self.params = {'ap': ap, 'tp': tp, 'atmp': atmp, 'cp': cdi, 'iop': iop, 'sp': sp, 'mp': mp}
 
         # show all the parameters input into the simulation (before some are updated by Telescope and Camera)
         if sp.verbose:
@@ -95,11 +96,11 @@ class RunMedis():
                 now = datetime.now().strftime("%m:%d:%Y_%H-%M-%S")
                 backup_testr = os.path.join(iop.datadir, self.name+'_backup_'+now)
                 if not sp.auto_load:
-                    choice = input(f'\n\n\tINPUT REQUIRED...\n\n'
+                    choice = input(f'\n\n\tINPUT REQUIRED...\n'
                                     f'Rename old testdir to {backup_testr} and start new simulation as {iop.testdir} [R],'
                                     f'\nor Quit [Q],'
                                     f'\nor overwrite the pickle file .pkl (keeps atmosphere and aberration maps) [P]?'
-                                    f'\nor ignore parameter difference and proceed with Loading original [L]?\n')
+                                    f'\nor ignore parameter difference and proceed with Loading original [L]?\n\n')
 
                     if choice.lower() == 'q':
                         exit()
@@ -116,10 +117,6 @@ class RunMedis():
                         # Overwrite the .pkl files but keep aberration and atmosphere directories
                         os.remove(f"{iop.testdir}/params.pkl")
 
-        # this is needed for both Telescope and Camera
-        if not isinstance(ap.n_wvl_final, int):
-            ap.n_wvl_final = ap.n_wvl_init
-
     def make_testdir(self):
         if not os.path.isdir(iop.testdir):
             os.makedirs(iop.testdir, exist_ok=True)
@@ -130,13 +127,14 @@ class RunMedis():
     def check_params(self):
         """ Check all param classes at this stage. Some params are updated in Telescope and Camera and those params
          should be checked against each other at that stage of the pipeline"""
-
+         #TODO check that the naming conventions of atmosphere and aberration maps match the new file format rather
+         #TODO than only checking the directory name
         with open(iop.params_logs, 'rb') as handle:
             loaded_params = pickle.load(handle)
 
         match_params = {}
         print(f"\nChecking Matching Params Classes:")
-        for p in ['ap','tp','atmp','cdip','iop','sp','mp']:
+        for p in ['ap','tp','atmp','iop','sp','mp','cp']:
             matches = []
             for (this_attr, this_val), (load_attr, load_val) in zip(self.params[p].__dict__.items(),
                                                                     loaded_params[p].__dict__.items()):
@@ -157,8 +155,7 @@ class RunMedis():
         return match_params
 
     def __call__(self, *args, **kwargs):
-        """ Get fields from Telescope and optionally then get photons from Camera. This funciton will generate and store
-         the photons but can't return them if they don't fit in memory. This looks complicated because of
+        """ Get fields from Telescope and optionally then get photons from Camera. This looks complicated because of
          the possibility to chunk in both Telescope and Camera but simplifies a lot if both have num_chunk = 1"""
 
         if self.product == 'fields':
@@ -166,7 +163,7 @@ class RunMedis():
             self.tel = Telescope(usesave=sp.save_to_disk)  # checking of class's cache etc is left to the class
 
             #load or generate the fields
-            observation = self.tel() # will just load first chunk if chunked, call tel() multiple times if you want more
+            observation = self.tel()
 
         else:
             self.cam = Camera(usesave=sp.save_to_disk, product=self.product)  # get the camera configuratoin
@@ -174,20 +171,20 @@ class RunMedis():
                 observation = self.cam()
             else:
                 self.tel = Telescope(usesave=sp.save_to_disk)
+                self.tel()
 
                 if self.tel.num_chunks == 1:
-                    fields = self.tel()['fields']
-                    observation = self.cam(fields)
+                    observation = self.cam(fields=self.tel.cpx_sequence)
                 else:
-                    for ichunk in range(int(np.ceil(self.tel.num_chunks))):
-                        # fields = self.tel.load_fields(span=(ichunk*self.tel.chunk_steps, (ichunk+1)*self.tel.chunk_steps))['fields']
-                        fields = self.tel()['fields']  # first call will create fields and load first chunk, subsequent will load next chunk
-                        # fields = fields[:, :, :, 0][:, :, :, np.newaxis]
-                        observation = self.cam(fields=fields, abs_step=ichunk*self.tel.chunk_steps, 
+                    for ichunk in range(int(self.tel.num_chunks)):
+                        fields = self.tel.load_fields(span=(ichunk*self.tel.chunk_steps, (ichunk+1)*self.tel.chunk_steps))['fields']
+
+                        observation = self.cam(fields=fields, abs_step=ichunk*self.tel.chunk_steps,
                                                finalise_photontable=False)
 
                     if self.product == 'photons' and not self.cam.photontable_exists:
-                        self.cam.populate_photontable(photons=[], finalise=True)
+                        self.cam.save_photontable(photonlist=[], index=('ultralight', 6), populate_subsidiaries=True)
+                        self.cam.photontable_exists = True
                         self.cam.save_instance()
 
                     print('Returning the observation data for the final chunk only')
